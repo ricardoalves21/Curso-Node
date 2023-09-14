b516e1572ff2231b02c68a1b43ffab9ff749f8ba
Ricardo Alves <dev.rical21@gmail.com>
2023-09-14 10:53:04 -0300

    Sobre promises, async e await

---
+++ index.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+
+//     const encoding = 'utf-8';
+
+//     fs.readFile(
+//         caminhoDoArquivo, 
+//         encoding, 
+//         (erro, texto) => { 
+//             if (erro) {
+//                 trataErro(erro);
+//             } else {
+//                 console.log(chalk.green(texto)); 
+//             }                
+//         })
+
+// }
+
+function pegaArquivo(caminhoDoArquivo) {
+
+    const encoding = 'utf-8';
+
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+
+}
+
+pegaArquivo('./arquivos/texto.md');
