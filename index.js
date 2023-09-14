b27b027500e714b03e5091e6901faeba5d43873d
Ricardo Alves <dev.rical21@gmail.com>
2023-09-14 11:16:32 -0300

    Async e Await

--- index.js
+++ index.js
@@ -5,32 +5,29 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
 }
 
+
+// PROMISES COM THEN():
+
 // function pegaArquivo(caminhoDoArquivo) {
 
 //     const encoding = 'utf-8';
 
-//     fs.readFile(
-//         caminhoDoArquivo, 
-//         encoding, 
-//         (erro, texto) => { 
-//             if (erro) {
-//                 trataErro(erro);
-//             } else {
-//                 console.log(chalk.green(texto)); 
-//             }                
-//         })
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
 
 // }
 
-function pegaArquivo(caminhoDoArquivo) {
-
-    const encoding = 'utf-8';
-
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch(trataErro)
-
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));   
+    } catch (erro) {
+        trataErro(erro);
+    }
 }
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
